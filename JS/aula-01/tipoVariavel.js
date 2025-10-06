2dc90178dfef21066ba99fc7e73e9929a9d5a313
Hugo Pamponet <hugofarias9@gmail.com>
2025-10-05 22:12:40 -0300

    Aula 01 de JS no dia 03/10/2025

--- JS/aula-01/tipoVariavel.js
+++ JS/aula-01/tipoVariavel.js
@@ -9,3 +9,19 @@ document.writeln(number)
 //Variável boleano(Verdade ou falso)
 let boleano = false;
 document.writeln(boleano)
+
+//undefined
+let novaVariavel
+document.writeln(novaVariavel)
+
+//nulo
+let valorNulo = null;
+document.writeln(valorNulo)
+
+/**
+ * Symbol
+ * Tipo de dado único e imutável, usado para criar identificadores únicos
+ * para propriedades de objetos.
+ */
+let simbolo = Symbol('descricao')
+document.writeln(simbolo)
