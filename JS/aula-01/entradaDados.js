c3600c8324a27986f23c2d0afc4522eb74e2a685
Hugo Pamponet <hugofarias9@gmail.com>
2025-10-05 22:32:24 -0300

    Aula 01 de JS no dia 03/10/2025

--- JS/aula-01/entradaDados.js
+++ JS/aula-01/entradaDados.js
@@ -2,5 +2,24 @@
 let idade = prompt("Digite sua idade")
 document.writeln(idade)
 
+//O confirma sempre vai ser um boleano
 let confirma = confirm("Qual é a idade digitada?")
 document.writeln(confirma)
+
+/**
+ * Diferença entre parseInt() e parseFloat()
+ * parseInt() corta as casas decimais
+ * parseFloat() as casas decimais permanecem
+ */
+
+let number1 = "55.33"
+
+//parseInt()
+document.writeln(parseInt(number1))
+
+//parseFloat()
+document.writeln(parseFloat(number1))
+
+//Para transformar um valor em string é usar o comando String()
+let number2 = 55
+document.writeln(String(number2))
